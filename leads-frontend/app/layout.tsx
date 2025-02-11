cb0d63e0e706f67c0ab54c22c89af17b89e32924
Xueming Xue <xxue@rippling.com>
2025-02-11 17:02:41 -0500

    setup mantine

--- leads-frontend/app/layout.tsx
+++ leads-frontend/app/layout.tsx
@@ -1,6 +1,11 @@
+import '@mantine/core/styles.css';
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import React from 'react';
+import { theme } from '../theme';
+import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +28,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
