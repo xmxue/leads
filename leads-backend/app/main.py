89bafdc396ac683f52da199dcde79ff0e3ae5b7a
Xueming <xmxue1018@gmail.com>
2025-02-14 02:10:22 -0500

    cors (#16)

--- leads-backend/app/main.py
+++ leads-backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 from app.router import router
 from app.db import create_db_and_tables
@@ -6,6 +7,14 @@ from app.db import create_db_and_tables
 create_db_and_tables()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health_check():
