02b3ffe916cb50ff79001776fb3c19e4c05c857f
Xueming Xue <xxue@rippling.com>
2025-02-14 14:49:16 -0500

    more logs

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export const metadata: Metadata = {
 };
 
 console.log('env', process.env.NODE_ENV);
+console.log('NEXT_PUBLIC_API_BASE_URL', process.env.NEXT_PUBLIC_API_BASE_URL);
 
 export default function RootLayout({
   children,
