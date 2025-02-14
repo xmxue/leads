eda27e398c4941751fed33e625e4a1fbac20eb34
Xueming <xmxue1018@gmail.com>
2025-02-14 11:36:03 -0500

    logging interceptor (#18)
    to debug POST 422 errors on Vercel but not locally

--- leads-backend/app/main.py
+++ leads-backend/app/main.py
@@ -1,18 +1,47 @@
-from fastapi import FastAPI
+from io import BytesIO
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 from app.router import router
 from app.db import create_db_and_tables
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import JSONResponse
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, style="{", format="{levelname} {message}")
+logger = logging.getLogger(__name__)
 
 create_db_and_tables()
 
 app = FastAPI()
 
+
+# Middleware to log request and response
+class LogRequestResponseMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        # Log request headers and body
+        request_body = await request.body()
+        logger.info(f"Request URL: {request.url}")
+        logger.info(f"Request Headers: {request.headers}")
+        logger.info(f"Request Body: {request_body.decode('utf-8')}")
+
+        # Call the next middleware or the route handler
+        response = await call_next(request)
+
+        # Capture and log the response body and headers
+        logger.info(f"Response Status Code: {response.status_code}")
+        logger.info(f"Response Headers: {response.headers}")
+
+        return response
+
+app.add_middleware(LogRequestResponseMiddleware)
+
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 @app.get("/health")
