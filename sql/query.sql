626da67ea490c7565ba06d3225ed536bf74ddafa
Xueming Xue <xxue@rippling.com>
2025-02-19 21:48:37 -0500

    out path

--- sql/query.sql
+++ sql/query.sql
@@ -1,2 +1,5 @@
 -- name: ListLeads :many
-SELECT first_name, last_name FROM lead where stage = $1 AND created_at > $2 ORDER BY created_at DESC;
+SELECT first_name, last_name FROM lead where stage = $1 AND created_at > @created_at_start AND created_at < @created_at_end AND first_name LIKE @first_name AND last_name LIKE @last_name;
+
+-- name: InsertLead :one
+INSERT INTO lead (first_name, last_name, email, phone, stage) VALUES ($1, $2, $3, $4, $5) RETURNING *;
